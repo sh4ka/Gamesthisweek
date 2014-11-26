a3605ee8d7f2dd7edb0a06fd0c89e9a493998ae9
Jesús Flores <jesus.flores@bqreaders.com>
2014-11-26 13:21:44 +0100

    This is not scraping

--- src/AppBundle/Command/FetchCommand.php
+++ src/AppBundle/Command/FetchCommand.php
@@ -8,22 +8,28 @@ namespace AppBundle\Command;
 
 use AppBundle\Entity\Game;
 use AppBundle\Entity\Image;
+use AppBundle\Entity\Platform;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Validator\Constraints\DateTime;
 
-class ScraperCommand extends ContainerAwareCommand {
+class FetchCommand extends ContainerAwareCommand {
 
     const PC_PLATFORM = 94;
+    const PS3_PLATFORM = 35;
+    const PS4_PLATFORM = 146;
+    const XBOX360_PLATFORM = 20;
+    const XBOXONE_PLATFORM = 145;
 
     protected $apiKey;
+    protected $logger; // monolog logger
 
     protected function configure()
     {
 
         $this
-            ->setName('app:scrape')
+            ->setName('app:fetch')
             ->setDescription('Get fresh new data')
         ;
     }
@@ -31,29 +37,64 @@ class ScraperCommand extends ContainerAwareCommand {
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->apiKey = $this->getContainer()->getParameter('giant_bomb_api_key');
-        $this->scrapePc();
+        $this->logger = $this->getContainer()->get('logger');
+        $this->fetchPc();
+        $this->fetchPS3();
+        $this->fetchPS4();
+        $this->fetchXbox360();
+        $this->fetchXboxOne();
     }
 
-    protected function scrapePc()
+    protected function fetchGeneric($platform)
     {
         $apiBaseUrl = 'http://www.giantbomb.com/api/releases/';
-        $platform = $this->getContainer()->get('doctrine')->getManager()->getRepository('AppBundle:Platform')->find(self::PC_PLATFORM);
         $date1week = date_create(date('Y-m-d'));
         date_add($date1week, date_interval_create_from_date_string('7 days'));
-        $filter = 'filter=release_date:'.date('Y-m-d').'|'.date('Y-m-d', $date1week->getTimestamp()).',platform:'.self::PC_PLATFORM;
+        $filter = 'filter=release_date:'.date('Y-m-d').'|'.date('Y-m-d', $date1week->getTimestamp()).',platform:'.$platform;
         $finalUrl = $apiBaseUrl.'?api_key='.$this->apiKey.'&format=json&'.$filter;
-        var_dump($finalUrl);
+        $this->logger->debug('Getting url: '.$finalUrl);
         $jsonData = file_get_contents($finalUrl);
-        $this->insertGameArray(json_decode($jsonData,TRUE), $platform);
+        return json_decode($jsonData,TRUE);
     }
 
-    protected function insertGameArray(array $games, $platform){
+    protected function getPlatform($platform){
+        $this->logger->debug('Getting platform: '.$platform);
+        return $this->getContainer()->get('doctrine')->getManager()->getRepository('AppBundle:Platform')->find($platform);
+    }
+
+    protected function fetchPc()
+    {
+        $this->insertGameArray($this->fetchGeneric(self::PC_PLATFORM), $this->getPlatform(self::PC_PLATFORM));
+    }
+
+    protected function fetchPS3()
+    {
+        $this->insertGameArray($this->fetchGeneric(self::PS3_PLATFORM), $this->getPlatform(self::PS3_PLATFORM));
+    }
+
+    protected function fetchPS4()
+    {
+        $this->insertGameArray($this->fetchGeneric(self::PS4_PLATFORM), $this->getPlatform(self::PS4_PLATFORM));
+    }
+
+    protected function fetchXbox360()
+    {
+        $this->insertGameArray($this->fetchGeneric(self::XBOX360_PLATFORM), $this->getPlatform(self::XBOX360_PLATFORM));
+    }
+
+    protected function fetchXboxOne()
+    {
+        $this->insertGameArray($this->fetchGeneric(self::XBOXONE_PLATFORM), $this->getPlatform(self::XBOXONE_PLATFORM));
+    }
+
+    protected function insertGameArray(array $games, Platform $platform){
         if(count($games) > 0){
             $em = $this->getContainer()->get('doctrine')->getManager();
             foreach($games['results'] as $game){
                 // find if it exists by name and platform
                 $existingGame = $em->getRepository('AppBundle:Game')->findOneBy(array('name' => $game['name'], 'platform' => $platform->getId()));
                 if(!$existingGame){
+                    $this->logger->debug('New '.$platform->getName().' game: '.$game['name']);
                     $newGame = new Game();
                     $newGame->setName($game['name']);
                     $newGame->setReleaseDay($game['expected_release_day']);
@@ -63,19 +104,24 @@ class ScraperCommand extends ContainerAwareCommand {
                     $em->persist($newGame);
                     $em->flush();
                     $this->processImages($game['image'], $newGame);
+                } else {
+                    $this->logger->debug('Existing '.$platform->getName().' game: '.$game['name']);
                 }
             }
         }
     }
 
-    protected function processImages($image, $game){
+    protected function processImages($image, Game $game){
         $em = $this->getContainer()->get('doctrine')->getManager();
         // save only thumb by now
         if(!empty($image['thumb_url'])){
+            $this->logger->debug('Found '.$game->getName().' image: '.$image['thumb_url']);
             $newImage = new Image();
             $newImage->setGame($game);
             $newImage->setUrl($image['thumb_url']);
             $em->persist($newImage);
+        } else {
+            $this->logger->debug('Image for: '.$game->getName().' not found');
         }
         $em->flush();
     }
